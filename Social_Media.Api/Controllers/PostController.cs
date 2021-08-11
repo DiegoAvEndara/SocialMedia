60f1328c73df228c38592e2d62b040dd5e905fed
Carlos Diego Avendaño Endara <avencarlosdiego@gmail.com>
2021-08-11 14:52:11 -0400

    Agregar archivos de proyecto.

---
+++ Social_Media.Api/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infraestructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Social_Media.Api.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class PostController : ControllerBase
+  {
+    public readonly IPostRepository _postRepository;
+    public PostController(IPostRepository postRepository)
+    {
+      _postRepository = postRepository;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetPosts()
+    {
+      var posts = await _postRepository.GetPosts();
+      return Ok(posts);
+    }
+  }
+}
---
+++ Social_Media.Api/Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infraestructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Social_Media.Api
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+      //Injección de dependencias (PostRepository o PostMongoRepository)
+      services.AddTransient<IPostRepository, PostMongoRepository>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
