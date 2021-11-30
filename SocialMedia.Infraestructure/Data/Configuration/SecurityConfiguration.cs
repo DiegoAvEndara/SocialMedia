6102ab4313eba109c66ae374350631782afcfe13
Carlos Diego Avendaño Endara <CAvendano@bnb.com.bo>
2021-11-29 23:47:12 -0400

    Register and login Users:

---
+++ SocialMedia.Infraestructure/Data/Configuration/SecurityConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialMedia.Infraestructure.Data.Configuration
+{
+  public class SecurityConfiguration: IEntityTypeConfiguration<Security>
+  {
+    public void Configure(EntityTypeBuilder<Security> builder)
+    {
+      builder.ToTable("Seguridad");
+      builder.HasKey(e => e.Id);
+
+      builder.Property(e => e.Id)
+        .HasColumnName("IdSeguridad");
+
+      builder.Property(e => e.User)
+        .HasColumnName("Usuario")
+        .HasMaxLength(50)
+        .IsUnicode(false);
+
+      builder.Property(e => e.UserName)
+        .HasColumnName("NombreUsuario")
+        .IsRequired()
+        .HasMaxLength(100)
+        .IsUnicode(false);
+
+      builder.Property(e => e.Password)
+        .HasColumnName("Contrasena")
+        .IsRequired()
+        .HasMaxLength(200)
+        .IsUnicode(false);
+
+      builder.Property(e => e.Role)
+        .HasColumnName("Rol")
+        .HasMaxLength(15)
+        .IsRequired()
+        .HasConversion(x => x.ToString(),
+        x => (RoleType)Enum.Parse(typeof(RoleType), x)
+        );
+    }
+  }
+}
---
+++ SocialMedia.Infraestructure/Repositories/SecurityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infraestructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infraestructure.Repositories
+{
+  public class SecurityRepository : BaseRepository<Security>, ISecurityRepository
+  {
+    public SecurityRepository(SocialMediaContext context) : base(context) { }
+    public async Task<Security> GetLoginByCredential(UserLogin login)
+    {
+      return await _entities.FirstOrDefaultAsync(x => x.User == login.User && x.Password == login.Password);
+    }
+  }
+}
